0aa7a695d76bc1ef8332ce63c9a52848ee50c800
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-26 17:31:54 +0100

    Add trainincrm_demo & demo cloud DBs

--- terraform.tf
+++ terraform.tf
@@ -57,3 +57,49 @@ resource "postgresql_database" "trainingcrm_sika_io" {
   connection_limit  = -1
   allow_connections = true
 }
+
+resource "postgresql_role" "trainingcrm_demo_sika_io" {
+  name     = "trainingcrm_demo_sika_io"
+  login    = true
+  password = "pisusisvojecrmvole"
+}
+
+
+resource "postgresql_database" "trainingcrm_demo_sika_io" {
+  name              = "trainingcrm_demo_sika_io"
+  owner             = postgresql_role.trainingcrm_sika_io.name
+  template          = "template0"
+  lc_collate        = "en_US.utf8"
+  lc_ctype          = "en_US.utf8"
+  connection_limit  = -1
+  allow_connections = true
+}
+
+
+resource "postgresql_role" "democloud" {
+  name     = "democloud"
+  login    = true
+  password = "democlouddemo"
+}
+
+
+resource "postgresql_database" "democloud" {
+  name              = "democloud"
+  owner             = postgresql_role.democloud.name
+  template          = "template0"
+  lc_collate        = "en_US.utf8"
+  lc_ctype          = "en_US.utf8"
+  connection_limit  = -1
+  allow_connections = true
+}
+
+resource "postgresql_database" "democloud_demo" {
+  name              = "democloud_demo"
+  owner             = postgresql_role.democloud.name
+  template          = "template0"
+  lc_collate        = "en_US.utf8"
+  lc_ctype          = "en_US.utf8"
+  connection_limit  = -1
+  allow_connections = true
+}
+
